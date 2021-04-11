731bcae69500d3ac0fa41e8a2b9fe8b1f49989b0
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-04-11 14:23:33 +0300

    get akan name according to name

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,17 @@ var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 //Create a function to extract date, month and year from entered dates of birth
 var day=["0", "1", "2", "3", "4", "5", "6"]
 function submitStaff(){
-    var date=form.birthday.value.toString()
-    var list=date.split("-")
-    console.log(list)
-    var cc=list(0).substring(0,2)
+    var date=new Date(form.birthday.value)
+    var day=date.getDay()
+    var result
+    var gender=form.gender.value
+    if(gender=="male"){
+        result=maleNames[day]
+    }
+    else if(gender=="female"){
+        result=femaleNames[day]
+    }
+    console.log(result)
 
-    // Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 }
 
