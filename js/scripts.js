aa96104c1c2ae83ba516a11672f3fd6f18716a9d
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-04-11 12:03:59 +0300

    add new js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+//Create a function to extract date, month and year from entered dates of birth
